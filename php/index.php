bdfe5363725a7d76a83fb1edab48ffa22516d074
Jeremy Bae <swbae@coupang.com>
2017-03-08 10:17:28 +0900

    Whitespace cleanup

--- php/index.php
+++ php/index.php
@@ -8,18 +8,18 @@
     <h1>Hello World!</h1>
     <p>
         <?php
-            $con = mysqli_connect(getenv('OPENSHIFT_MYSQL_DB_HOST'), getenv('OPENSHIFT_MYSQL_DB_USERNAME'), 
+            $con = mysqli_connect(getenv('OPENSHIFT_MYSQL_DB_HOST'), getenv('OPENSHIFT_MYSQL_DB_USERNAME'),
               getenv('OPENSHIFT_MYSQL_DB_PASSWORD'), getEnv('OPENSHIFT_APP_NAME'), getEnv('OPENSHIFT_MYSQL_DB_PORT'));
             if (mysqli_connect_errno()) {
-                echo "Database connection failed: " . mysqli_connect_error();	
+                echo "Database connection failed: " . mysqli_connect_error();
                 exit();
             }
-	
+
             $result = mysqli_query($con, "SELECT * FROM Place");
 
-            if (mysqli_num_rows($result)==0) {	
+            if (mysqli_num_rows($result)==0) {
                 echo "No places to display. Please add some to the database.";
-	    } else {
+        } else {
                 echo "Warm greetings in particular to:";
                 echo "<ul>";
                 while($row = mysqli_fetch_array($result)) {
